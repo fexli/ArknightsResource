94a9d73b23ac763df0b0f0c850a91de62c8b65a7
fexli <xiliang2002@sina.com>
2024-03-07 12:14:50 +0800

    Client:2.2.21|Res:24-03-06-07-46-40-d13e0f

--- gamedata/[uc]lua/feature/featuremodule.lua
+++ gamedata/[uc]lua/feature/featuremodule.lua
@@ -1,88 +1,89 @@
-require "Feature/ResDefines"
-require "Feature/AudioConsts"
-require "Feature/Supportor/SupportorModule"
-
-require "Feature/Main/MainDlg"
-require "Feature/Main/TestDlg"
-
-require "Feature/Activity/Collection/CollectionItem"
-require "Feature/Activity/Collection/CollectionMainDlg"
-require "Feature/Activity/Collection/CollectionTaskListDlg"
-require "Feature/Activity/Collection/CollectionActModel"
-require "Feature/Activity/Collection/CollectionDailyTaskItem"
-require "Feature/Activity/Collection/CollectionTimedTaskItem"
-require "Feature/Activity/LoginOnly/LoginOnlyDefine"
-require "Feature/Activity/LoginOnly/LoginOnlyDlg"
-require "Feature/Activity/LoginOnly/NoEntryLoginOnlyDlg"
-
-require "Feature/Activity/PrayOnly/PrayOnlyDefine"
-require "Feature/Activity/PrayOnly/PrayOnlyMainDlg"
-require "Feature/Activity/PrayOnly/PrayOnlyActDefines"
-
-require "Feature/Activity/ActFlip/ActFlipMainDlg"
-
-require "Feature/Activity/LuaActivityUtil"
-
-require "Feature/Activity/Act3D5/Act3D5Dlg"
-require "Feature/Activity/Act4D5/Act4D5Dlg"
-require "Feature/Activity/Act6D8/Act6D8Dlg"
-require "Feature/Activity/Act9D1D2/Act9D1D2Dlg"
-require "Feature/Activity/Act9D4/Act9D4Dlg"
-require "Feature/Activity/Act14D0D2/Act14D0D2Dlg"
-require "Feature/Activity/Act16D5D1/Act16D5D1Dlg"
-
-require "Feature/Activity/GridGacha/GridGachaDefine"
-require "Feature/Activity/GridGacha/GridGachaMainDlg"
-
-require "Feature/Activity/GridGachaV2/GridGachaV2Define"
-require "Feature/Activity/GridGachaV2/GridGachaV2MainDlg"
-
-require "Feature/Activity/FloatParade/FloatParadeProbDetailPanel"
-require "Feature/Activity/FloatParade/FloatParadeRulePanel"
-require "Feature/Activity/FloatParade/FloatParadeRewardRulePanel"
-require "Feature/Activity/FloatParade/FloatParadeRecycleDayListAdapter"
-require "Feature/Activity/FloatParade/FloatParadeTacticTab"
-require "Feature/Activity/FloatParade/FloatParadeMainDlg"
-
-require "Feature/Activity/CheckinAllPlayer/CheckinAllPlayerMainDlg"
-require "Feature/Activity/CheckinVs/CheckinVsMainDlg"
-
-require "Feature/Activity/MainlineBuff/MainlineBuffDefine"
-require "Feature/Activity/MainlineBuff/MainlineBuffMainDlg"
-
-require "Feature/Wardrobe/WardrobeModule"
-
-require "Feature/Operation/ReturnV2/ReturnV2Model";
-require "Feature/Operation/ReturnV2/ReturnV2MainDlgViewModel"
-require "Feature/Operation/ReturnV2/ReturnV2MainDlg";
-require "Feature/Operation/ReturnV2/ReturnV2Define";
-require "Feature/Operation/ReturnV2/ReturnV2WelcomeDlg"
-require "Feature/Operation/ReturnV2/ReturnV2RewardsDlg"
-require "Feature/Operation/ReturnV2/ReturnV2TrackPoint";
-
-require "Feature/Operation/Returnning/ReturnDefine"
-require "Feature/Operation/Returnning/ReturnService"
-require "Feature/Operation/Returnning/ReturnModel"
-require "Feature/Operation/Returnning/ReturnTrackPoint"
-require "Feature/Operation/Returnning/ReturnPageDlg"
-require "Feature/Operation/Returnning/ReturnMainDlg"
-require "Feature/Operation/Returnning/ReturnWelcomeDlg"
-require "Feature/Operation/Returnning/ReturnRewardsDlg"
-
-require "Feature/Operation/ActFun/ActFunModule"
-
-require "Feature/Activity/SwitchOnly/SwitchOnlyDefine"
-require "Feature/Activity/SwitchOnly/SwitchOnlyDlg"
-require "Feature/Activity/SwitchOnly/SwitchOnlyItemView"
-
-require "Feature/Activity/UniqueOnly/UniqueOnlyDefine"
-require "Feature/Activity/UniqueOnly/UniqueOnlyDlg"
-require "Feature/Activity/UniqueOnly/UniqueOnlyUtil"
-
-require "Feature/Activity/MainlineBp/MainlineBpDefine";
-require "Feature/Activity/MainlineBp/MainlineBpUtil";
-require "Feature/Activity/MainlineBp/MainlineBpMainDlg";
-
-require "Feature/Activity/BlessOnly/BlessOnlyDefine"
-require "Feature/Activity/BlessOnly/BlessOnlyUtil"
-require "Feature/Activity/BlessOnly/BlessOnlyMainDlg"
+require "Feature/ResDefines"
+require "Feature/AudioConsts"
+require "Feature/Supportor/SupportorModule"
+
+require "Feature/Main/MainDlg"
+require "Feature/Main/TestDlg"
+
+require "Feature/Activity/Collection/CollectionItem"
+require "Feature/Activity/Collection/CollectionMainDlg"
+require "Feature/Activity/Collection/CollectionTaskListDlg"
+require "Feature/Activity/Collection/CollectionActModel"
+require "Feature/Activity/Collection/CollectionDailyTaskItem"
+require "Feature/Activity/Collection/CollectionTimedTaskItem"
+require "Feature/Activity/LoginOnly/LoginOnlyDefine"
+require "Feature/Activity/LoginOnly/LoginOnlyDlg"
+require "Feature/Activity/LoginOnly/NoEntryLoginOnlyDlg"
+
+require "Feature/Activity/PrayOnly/PrayOnlyDefine"
+require "Feature/Activity/PrayOnly/PrayOnlyMainDlg"
+require "Feature/Activity/PrayOnly/PrayOnlyActDefines"
+
+require "Feature/Activity/ActFlip/ActFlipMainDlg"
+
+require "Feature/Activity/LuaActivityUtil"
+
+require "Feature/Activity/Act3D5/Act3D5Dlg"
+require "Feature/Activity/Act4D5/Act4D5Dlg"
+require "Feature/Activity/Act6D8/Act6D8Dlg"
+require "Feature/Activity/Act9D1D2/Act9D1D2Dlg"
+require "Feature/Activity/Act9D4/Act9D4Dlg"
+require "Feature/Activity/Act14D0D2/Act14D0D2Dlg"
+require "Feature/Activity/Act16D5D1/Act16D5D1Dlg"
+
+require "Feature/Activity/GridGacha/GridGachaDefine"
+require "Feature/Activity/GridGacha/GridGachaMainDlg"
+
+require "Feature/Activity/GridGachaV2/GridGachaV2Define"
+require "Feature/Activity/GridGachaV2/GridGachaV2MainDlg"
+
+require "Feature/Activity/FloatParade/FloatParadeProbDetailPanel"
+require "Feature/Activity/FloatParade/FloatParadeRulePanel"
+require "Feature/Activity/FloatParade/FloatParadeRewardRulePanel"
+require "Feature/Activity/FloatParade/FloatParadeRecycleDayListAdapter"
+require "Feature/Activity/FloatParade/FloatParadeTacticTab"
+require "Feature/Activity/FloatParade/FloatParadeMainDlg"
+
+require "Feature/Activity/CheckinAllPlayer/CheckinAllPlayerMainDlg"
+require "Feature/Activity/CheckinVs/CheckinVsMainDlg"
+
+require "Feature/Activity/MainlineBuff/MainlineBuffDefine"
+require "Feature/Activity/MainlineBuff/MainlineBuffMainDlg"
+
+require "Feature/Wardrobe/WardrobeModule"
+
+require "Feature/Operation/ReturnV2/ReturnV2Model";
+require "Feature/Operation/ReturnV2/ReturnV2MainDlgViewModel"
+require "Feature/Operation/ReturnV2/ReturnV2MainDlg";
+require "Feature/Operation/ReturnV2/ReturnV2Define";
+require "Feature/Operation/ReturnV2/ReturnV2WelcomeDlg"
+require "Feature/Operation/ReturnV2/ReturnV2RewardsDlg"
+require "Feature/Operation/ReturnV2/ReturnV2TrackPoint";
+
+require "Feature/Operation/Returnning/ReturnDefine"
+require "Feature/Operation/Returnning/ReturnService"
+require "Feature/Operation/Returnning/ReturnModel"
+require "Feature/Operation/Returnning/ReturnTrackPoint"
+require "Feature/Operation/Returnning/ReturnPageDlg"
+require "Feature/Operation/Returnning/ReturnMainDlg"
+require "Feature/Operation/Returnning/ReturnWelcomeDlg"
+require "Feature/Operation/Returnning/ReturnRewardsDlg"
+
+
+require "Feature/Activity/SwitchOnly/SwitchOnlyDefine"
+require "Feature/Activity/SwitchOnly/SwitchOnlyDlg"
+require "Feature/Activity/SwitchOnly/SwitchOnlyItemView"
+
+require "Feature/Activity/UniqueOnly/UniqueOnlyDefine"
+require "Feature/Activity/UniqueOnly/UniqueOnlyDlg"
+require "Feature/Activity/UniqueOnly/UniqueOnlyUtil"
+
+require "Feature/Activity/MainlineBp/MainlineBpDefine";
+require "Feature/Activity/MainlineBp/MainlineBpUtil";
+require "Feature/Activity/MainlineBp/MainlineBpMainDlg";
+
+require "Feature/Activity/BlessOnly/BlessOnlyDefine"
+require "Feature/Activity/BlessOnly/BlessOnlyUtil"
+require "Feature/Activity/BlessOnly/BlessOnlyMainDlg"
+
+require "Feature/SandboxV2/SandboxV2DiffDialog"
