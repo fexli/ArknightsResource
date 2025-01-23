d45584ef469aaef204159901b0a2e61f7a85f14e
fexli <xiliang2002@sina.com>
2025-01-24 01:00:55 +0800

    Client:2.4.41|Res:25-01-23-12-42-24-1e19eb

--- gamedata/[uc]lua/hotfixes/definedfix.lua
+++ gamedata/[uc]lua/hotfixes/definedfix.lua
@@ -2,7 +2,14 @@
 local list = 
 {
   
+  "HotFixes/VCameraControllerHotfixer",
+  "HotFixes/BuildingControllerfixer",
   "HotFixes/TestStubHotfixer",
+  "HotFixes/ArchiveRelicModelHotfixer",
+  "HotFixes/RoguelikeActivityHistorySeedItemModelHotfixer",
+  "HotFixes/EtlchiHotfixer",
+  "HotFixes/StagePageHotfixer",
+  "HotFixes/CheckTheLeftSameDeployedTokenHotFixer",
 };
 
 return list;
